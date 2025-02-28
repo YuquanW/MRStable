12b76e97684a6d6f16141e7dcf5f2f6f2b9bb8ac
YuquanW <21110700112@m.fudan.edu.cn>
2025-02-28 14:47:06 +0800

    try mcp

--- R/main.R
+++ R/main.R
@@ -86,21 +86,21 @@ MRStable_V <- function(beta_exp,
 #' @rdname MRStable
 #' @export
 
-ldsc_divw <- function(beta_exp, beta_out, se_exp, se_out, scale_exp, scale_out, over.dispersion = T) {
+ldsc_divw <- function(beta_exp, beta_out, se_exp, se_out, scale_exp, scale_out, over.dispersion = F) {
   m <- length(beta_exp)
   se_exp <- sqrt(scale_exp)*se_exp
-  se_out <- sqrt(scale_out)*se_out
+  se_out <- sqrt(scale_out*se_out^2)
   .divw(beta_exp, beta_out, se_exp, se_out, over.dispersion)
 }
 
 #' @rdname MRStable
 #' @export
 
-ada_ldsc_divw <- function(beta_exp, beta_out, se_exp, se_out, scale_exp, scale_out, n, maxit = 10000) {
+ada_ldsc_divw <- function(beta_exp, beta_out, se_exp, se_out, scale_exp, scale_out, n, over.dispersion = F, maxit = 10000) {
   m <- length(beta_exp)
   #nlam <- 1#50
   se_exp <- sqrt(scale_exp)*se_exp
-  se_out <- sqrt(scale_out)*se_out
+  se_out <- sqrt(scale_out*se_out^2)
   divw.init <- ldsc_divw(beta_exp, beta_out, se_exp, se_out, 1, 1)
   beta.hat <- beta.init <- divw.init$beta.hat
   #beta.se <- divw.init$beta.se
@@ -147,9 +147,67 @@ ada_ldsc_divw <- function(beta_exp, beta_out, se_exp, se_out, scale_exp, scale_o
   divw.res <- ldsc_divw(beta_exp[iv.valid],
                         beta_out[iv.valid],
                         se_exp[iv.valid],
-                        se_out[iv.valid], 1, 1)
+                        se_out[iv.valid], 1, 1,
+                        over.dispersion = over.dispersion)
   list(beta.hat = divw.res$beta.hat,
        beta.se = divw.res$beta.se,
        beta.p.value = divw.res$beta.p.value,
        iv.invalid = which(alpha.hat!=0))
 }
+
+#' @rdname MRStable
+#' @export
+
+ldsc_mcp_divw <- function(beta_exp, beta_out, se_exp, se_out, scale_exp, scale_out, n, a = 3, maxit = 10000) {
+  m <- length(beta_exp)
+  nlam <- 50
+  se_exp <- sqrt(scale_exp)*se_exp
+  se_out <- sqrt(scale_out)*se_out
+  divw.init <- ldsc_divw(beta_exp, beta_out, se_exp, se_out, 1, 1)
+  beta.init <- divw.init$beta.hat
+  alpha.init <- beta_out - beta.init*beta_exp
+  ny <- min(1/se_out^2)
+  lambda <- exp(seq(-0.5*log(n), 0, length.out = nlam))
+  bic <- NULL
+  alpha.all <- matrix(0, m, nlam)
+  for (i in 1:nlam) {
+    beta.hat <- beta.init
+    alpha.hat <- alpha.init
+    for (j in 1:maxit) {
+      beta.old <- beta.hat
+      gamma <- (beta.old*(beta_out-alpha.hat)/se_out^2+beta_exp/se_exp^2)/(beta.old^2/se_out^2+1/se_exp^2)
+      alpha.hat <- (abs(beta_out-beta.old*gamma)-ny*lambda[i]*se_out^2)*
+        (abs(beta_out-beta.old*gamma)>ny*lambda[i]*se_out^2)*
+        (abs(beta_out-beta.old*gamma)<=a*lambda[i])*
+        sign(beta_out-beta.old*gamma)/
+        (1-ny/a*se_out^2)+
+        (beta_out-beta.old*gamma)*(abs(beta_out-beta.old*gamma)>a*lambda[i])
+      beta.hat <- sum((beta_out-alpha.hat)*gamma/se_out^2)/sum(gamma^2/se_out^2)
+      if (abs(beta.hat - beta.old)/abs(beta.old) < 1e-7) {
+        break
+      }
+    }
+    valid.iv <- which(alpha.hat==0)
+    fn_beta <- function(beta) {
+      sum((beta_out[valid.iv] - beta*beta_exp[valid.iv])^2/
+            (beta^2*se_exp[valid.iv]^2+se_out[valid.iv]^2))
+    }
+    sol <- optim(beta.init, fn_beta, method = "BFGS")
+    ll <- sol$value
+    bic <- c(bic, ll+log(n)*sum(alpha.hat!=0))
+    alpha.all[, i] <- alpha.hat
+  }
+  plot(lambda, bic)
+  lambda.final <- lambda[which.min(bic)]
+  alpha.final <- alpha.all[, which.min(bic)]
+  iv.valid <- which(alpha.final==0)
+  divw.res <- ldsc_divw(beta_exp[iv.valid],
+                        beta_out[iv.valid],
+                        se_exp[iv.valid],
+                        se_out[iv.valid], 1, 1)
+  list(beta.hat = divw.res$beta.hat,
+       beta.se = divw.res$beta.se,
+       beta.p.value = divw.res$beta.p.value,
+       iv.invalid = which(alpha.final!=0))
+}
+
